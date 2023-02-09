4d5bf423242aaa44a65723b5150eb78b63168ac1
KatieInnes <katharine.innes@gmail.com>
2023-02-09 12:45:25 -0800

    init

--- src/App.js
+++ src/App.js
@@ -3,22 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <ul>
+        <li>Grocery shopping</li>
+        <li>Bring blueberries for Hearts Day at preschool</li>
+        <li>Put away Christmas wrapping paper</li>
+        <li>Add spring soccer practices and games to Family Calendar</li>
+      </ul>
+    </>
   );
 }
 
